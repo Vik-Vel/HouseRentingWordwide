239de7d3088c3ea931c1e5eae7bf23dc88831ea2
Viktoria Veleva <vikiveleva54@gmail.com>
2024-03-28 20:33:24 +0200

    IHouseService Update
    Add Tasks - HasAgentWithIdAsync, GetHouseFormModelByIdAsync
    
    Edit Task - Edit -> EditAsync

--- HouseRentingSystem.Core/Contracts/IHouseService.cs
+++ HouseRentingSystem.Core/Contracts/IHouseService.cs
@@ -31,6 +31,10 @@ namespace HouseRentingSystem.Core.Contracts
 
         Task<HouseDetailsServiceModel> HouseDetailsByIdAsync(int id);
 
-        Task Edit(int houseId, HouseFormModel model);
+        Task EditAsync(int houseId, HouseFormModel model);
+
+        Task<bool> HasAgentWithIdAsync(int houseId, string userId);
+
+        Task<HouseFormModel?> GetHouseFormModelByIdAsync(int id);
     }
 }
